a0152ac15df6669f541ae299a3160925d4208f60
EricZzz <biggogten@gmail.com>
2026-08-07 13:14:53 +0800

    feat: using Templates

--- main.go
+++ main.go
@@ -1,99 +1,32 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"time"
-
-	_ "github.com/go-sql-driver/mysql"
+	"html/template"
+	"net/http"
 )
 
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
 func main() {
-	db, err := sql.Open("mysql", "root:password@(127.0.0.1:3306)/go_web_examples_db?parseTime=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Create a new table
-	{
-		query := `CREATE TABLE IF NOT EXISTS users (
-                id INT AUTO_INCREMENT,
-                username TEXT NOT NULL,
-                password TEXT NOT NULL,
-                created_at DATETIME,
-                PRIMARY KEY (id)
-            );`
-
-		if _, err := db.Exec(query); err != nil {
-			log.Fatal(err)
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: false},
+			},
 		}
-	}
-
-	// Insert a new user
-	{
-		username := "eric"
-		password := "password123"
-		createdAt := time.Now()
-
-		result, err := db.Exec(`INSERT INTO users(username,password,created_at) VALUES (?,?,?)`, username, password, createdAt)
-		if err != nil {
-			log.Fatal(err)
-		}
-		id, err := result.LastInsertId()
-		fmt.Println(id)
-	}
-
-	// Query as single user
-	{
-		var (
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		)
-
-		query := "SELECT id,username,password,created_at FROM users WHERE id = ?"
-		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(id, username, password, createdAt)
-	}
-
-	// Query all users
-	{
-		type user struct {
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		}
-
-		rows, err := db.Query(`SELECT id,username,password,created_at FROM users`)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		var users []user
-		for rows.Next() {
-			var u user
-
-			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			users = append(users, u)
-		}
-		if err := rows.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%#v", users)
-	}
-
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":80", nil)
 }
