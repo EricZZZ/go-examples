1c16756b9111282807b31a9e0d18a61a8727c07a
EricZzz <biggogten@gmail.com>
2026-08-07 13:22:33 +0800

    feat: Assets and Files

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type TodoPageData struct {
 	Todos     []Todo
 }
 
-func main() {
+func mains() {
 	tmpl := template.Must(template.ParseFiles("layout.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := TodoPageData{
